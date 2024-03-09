86307967cd02da5a533684b552bc9fab01dc3302
Rerechan Store <139312654+Rerechan-Store@users.noreply.github.com>
2024-03-09 11:46:53 +0700

    Create lmenu.sh

---
+++ l2tp/lmenu.sh
@@ -0,0 +1,348 @@
+#!/bin/bash
+
+clear
+
+# Rerechan02
+
+clear
+
+addl() {
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+clear
+domain=$(cat /etc/xray/domain)
+clear
+until [[ $VPN_USER =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "Username: " -e VPN_USER
+		CLIENT_EXISTS=$(grep -w $VPN_USER /var/lib/premium-script/data-user-l2tp | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo "A client with the specified name was already created, please choose another name."
+			exit 1
+		fi
+	done
+read -p "Password: " VPN_PASSWORD
+read -p "Expired (days): " masaaktif
+exp=`date -d "$masaaktif days" +"%d-%m-%Y"`
+created=`date -d "0 days" +"%d-%m-%Y"`
+clear
+
+# Add or update VPN user
+cat >> /etc/ppp/chap-secrets <<EOF
+"$VPN_USER" l2tpd "$VPN_PASSWORD" *
+EOF
+
+VPN_PASSWORD_ENC=$(openssl passwd -1 "$VPN_PASSWORD")
+cat >> /etc/ipsec.d/passwd <<EOF
+$VPN_USER:$VPN_PASSWORD_ENC:xauth-psk
+EOF
+clear
+# Update file attributes
+chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
+echo -e "### $VPN_USER $exp">>"/var/lib/premium-script/data-user-l2tp"
+cat <<EOF
+L2TP/Ipsec Psk VPN Acc Information
+====================================
+Domain       : ${domain}
+IPsec PSK    : myvpn
+Username     : $VPN_USER
+Password     : $VPN_PASSWORD
+Expired      : $exp
+====================================
+EOF
+}
+
+addp() {
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(curl ifconfig.me)
+clear
+until [[ $VPN_USER =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "Username: " -e VPN_USER
+		CLIENT_EXISTS=$(grep -w $VPN_USER /var/lib/premium-script/data-user-pptp | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo "A client with the specified name was already created, please choose another name."
+			exit 1
+		fi
+	done
+read -p "Password: " VPN_PASSWORD
+read -p "Expired (days): " masaaktif
+exp=`date -d "$masaaktif days" +"%d-%m-%Y"`
+created=`date -d "0 days" +"%d-%m-%Y"`
+clear
+
+# Add or update VPN user
+cat >> /etc/ppp/chap-secrets <<EOF
+"$VPN_USER" pptpd "$VPN_PASSWORD" *
+EOF
+
+# Update file attributes
+chmod 600 /etc/ppp/chap-secrets*
+echo -e "### $VPN_USER $exp">>"/var/lib/premium-script/data-user-pptp"
+cat <<EOF
+
+================================
+PPTP VPN
+Server IP    : $MYIP
+Username     : $VPN_USER
+Password     : $VPN_PASSWORD
+Expired      : $exp
+=================================
+EOF
+}
+
+deltp() {
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+clear
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/premium-script/data-user-l2tp")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	echo ""
+	echo " Select the existing client you want to remove"
+	echo " Press CTRL+C to return"
+	echo " ==============================="
+	echo "     No  Expired   User"
+	grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 2-3 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select One Client[1]: " CLIENT_NUMBER
+		else
+			read -rp "Select One Client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
+	# match the selected number to a client name
+	VPN_USER=$(grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+	exp=$(grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+# Delete VPN user
+sed -i '/^"'"$VPN_USER"'" l2tpd/d' /etc/ppp/chap-secrets
+# shellcheck disable=SC2016
+sed -i '/^'"$VPN_USER"':\$1\$/d' /etc/ipsec.d/passwd
+sed -i "/^### $VPN_USER $exp/d" /var/lib/premium-script/data-user-l2tp
+# Update file attributes
+chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
+clear
+echo " L2TP Account Has Been Successfully Deleted"
+echo " =========================="
+echo " Client Name : $VPN_USER"
+echo " Expired On  : $exp"
+echo " =========================="
+}
+
+delptp() {
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+clear
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/premium-script/data-user-pptp")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	echo ""
+	echo " Select the existing client you want to remove"
+	echo " Press CTRL+C to return"
+	echo " ==============================="
+	echo "     No  Expired   User"
+	grep -E "^### " "/var/lib/premium-script/data-user-pptp" | cut -d ' ' -f 2-3 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select One Client[1]: " CLIENT_NUMBER
+		else
+			read -rp "Select One Client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
+	# match the selected number to a client name
+	VPN_USER=$(grep -E "^### " "/var/lib/premium-script/data-user-pptp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+	exp=$(grep -E "^### " "/var/lib/premium-script/data-user-pptp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+# Delete VPN user
+sed -i '/^"'"$VPN_USER"'" pptpd/d' /etc/ppp/chap-secrets
+sed -i "/^### $VPN_USER $exp/d" /var/lib/premium-script/data-user-pptp
+# Update file attributes
+chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
+clear
+echo " PPTP Account Has Been Successfully Deleted"
+echo " =========================="
+echo " Client Name : $VPN_USER"
+echo " Expired On  : $exp"
+echo " =========================="
+}
+
+renl() {
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/premium-script/data-user-l2tp")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		clear
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo "Select the existing client you want to renew"
+	echo " Press CTRL+C to return"
+	echo -e "==============================="
+	grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 2-3 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
+read -p "Expired (days): " masaaktif
+user=$(grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/var/lib/premium-script/data-user-l2tp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+now=$(date +%Y-%m-%d)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+exp3=$(($exp2 + $masaaktif))
+exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
+sed -i "s/### $user $exp/### $user $exp4/g" /var/lib/premium-script/data-user-l2tp
+clear
+echo ""
+echo " L2TP Account Has Been Successfully Renewed"
+echo " =========================="
+echo " Client Name : $user"
+echo " Expired On  : $exp4"
+echo " =========================="
+}
+
+renp() {
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+clear
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/premium-script/data-user-pptp")
+	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+		clear
+		echo ""
+		echo "You have no existing clients!"
+		exit 1
+	fi
+
+	clear
+	echo ""
+	echo "Select the existing client you want to renew"
+	echo " Press CTRL+C to return"
+	echo -e "==============================="
+	grep -E "^### " "/var/lib/premium-script/data-user-pptp" | cut -d ' ' -f 2-3 | nl -s ') '
+	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
+		if [[ ${CLIENT_NUMBER} == '1' ]]; then
+			read -rp "Select one client [1]: " CLIENT_NUMBER
+		else
+			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
+		fi
+	done
+read -p "Expired (days): " masaaktif
+user=$(grep -E "^### " "/var/lib/premium-script/data-user-pptp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/var/lib/premium-script/data-user-pptp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+now=$(date +%Y-%m-%d)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+exp3=$(($exp2 + $masaaktif))
+exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
+sed -i "s/### $user $exp/### $user $exp4/g" /var/lib/premium-script/data-user-pptp
+clear
+echo ""
+echo " PPTP Account Has Been Successfully Renewed"
+echo " =========================="
+echo " Client Name : $user"
+echo " Expired On  : $exp4"
+echo " =========================="
+}
+
+cekp() {
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+clear
+last | grep ppp | grep still | awk '{print " ",$1," - " $3 }' > /tmp/login-db-pptp.txt;
+echo " "
+echo " "
+echo "===========================================";
+echo " "
+echo " "
+echo "-------------------------------------";
+echo "    PPTP VPN User Login";
+echo "-------------------------------------";
+echo "Username   ---   IP";
+echo "-------------------------------------";
+cat /tmp/login-db-pptp.txt
+echo " "
+echo " "
+echo " "
+echo "===========================================";
+echo " ";
+rm -fr /tmp/login-db-pptp.txt
+}
+
+menu() {
+clear
+xp
+clear
+
+figlet L2TP | lolcat
+echp -e "
+==============================================
+++++++++++++++++++++++++++++++++++++++++++++++
+=============================================="
+sleep 1
+clear
+
+echo -e "
+==============================
+[ <= L2TP & PPTP MENU VPN => ]
+==============================
+
+1. Create Akaun L2TP
+2. Create Akaun PPTP
+3. Delete Akaun L2TP
+4. Delete Akaun PPTP
+5. Renew Akaun L2TP
+6. Renew Akaun PPTP
+7. Cek User Login PPTP
+==============================
+
+X) Back To Menu ALL VPN
+==============================
+"
+read -p "Select From Options [1-7 or x] :  " sma
+case $sma in
+1) clear ; xp ; addl ;;
+2) clear ; xp ; addp ;;
+3) clear ; xp ; deltp ;;
+4) clear ; xp ; delptp ;;
+5) clear ; xp ; renl ;;
+6) clear ; xp ; renp ;;
+7) clear ; xp ; cekp ;;
+x) clear ; xp ; funny ;;
+*) clear ; echo  "Please enter an correct number" ;;
+esac
+}
+
+
+
+
+
